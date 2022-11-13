f6d0258df1f581163a99da7f1669705ecec4242f
Zhe Liu <tina.liuzhe@gmail.com>
2022-11-12 20:05:57 -0800

    tabs to spaces

--- cover_letters.py
+++ cover_letters.py
@@ -13,44 +13,44 @@ SIGNATURE_FILE_NAME = 'Signature_File.png'
 
 
 with open(CSV_FILE_NAME, 'r') as csv_file:
-	reader = csv.reader(csv_file)
-	
-	# Get the header and skip over it
-	header = next(reader)
+    reader = csv.reader(csv_file)
+    
+    # Get the header and skip over it
+    header = next(reader)
 
-	for row in reader:
-		# Write the csv file needed for pdf generation
-		with open(CSV_FILE_AUX_NAME, 'w') as write_handler:
-			aux_writer = csv.writer(write_handler)
-			aux_writer.writerow(header)
-			aux_writer.writerow(row[:-1])
+    for row in reader:
+        # Write the csv file needed for pdf generation
+        with open(CSV_FILE_AUX_NAME, 'w') as write_handler:
+            aux_writer = csv.writer(write_handler)
+            aux_writer.writerow(header)
+            aux_writer.writerow(row[:-1])
 
-		# Generate the pdf
-		result = subprocess.run(
-			['pdflatex', TEX_FILE_NAME],
-		)
+        # Generate the pdf
+        result = subprocess.run(
+            ['pdflatex', TEX_FILE_NAME],
+        )
 
-		# Find a good name for the pdf file
-		institution = row[0].replace(' ', '_')
-		position = row[1].replace(' ', '_')
-		pdf_title = f'{institution}_{position}_cover.pdf'
+        # Find a good name for the pdf file
+        institution = row[0].replace(' ', '_')
+        position = row[1].replace(' ', '_')
+        pdf_title = f'{institution}_{position}_cover.pdf'
 
-		if SEPARATE_FOLDERS:
-			folder = institution
+        if SEPARATE_FOLDERS:
+            folder = institution
 
-			pdf_title = f'{folder}/{institution}_{position}_cover.pdf'
+            pdf_title = f'{folder}/{institution}_{position}_cover.pdf'
 
-			if not os.path.exists(folder):
-				os.mkdir(folder)
+            if not os.path.exists(folder):
+                os.mkdir(folder)
 
-			shutil.copyfile(CSV_FILE_AUX_NAME, f"{folder}/{CSV_FILE_AUX_NAME}")
-			shutil.copyfile(TEX_FILE_NAME, f"{folder}/{TEX_FILE_NAME}")
-			shutil.copyfile(SIGNATURE_FILE_NAME, f"{folder}/{SIGNATURE_FILE_NAME}")
-		else:
-			idx = 2
-			while os.path.exists(pdf_title):
-				pdf_title = f'{institution}_{position}_cover_{idx}.pdf'
-				idx += 1
+            shutil.copyfile(CSV_FILE_AUX_NAME, f"{folder}/{CSV_FILE_AUX_NAME}")
+            shutil.copyfile(TEX_FILE_NAME, f"{folder}/{TEX_FILE_NAME}")
+            shutil.copyfile(SIGNATURE_FILE_NAME, f"{folder}/{SIGNATURE_FILE_NAME}")
+        else:
+            idx = 2
+            while os.path.exists(pdf_title):
+                pdf_title = f'{institution}_{position}_cover_{idx}.pdf'
+                idx += 1
 
-		# Rename the pdf file
-		os.rename(PDF_FILE_NAME, pdf_title)
+        # Rename the pdf file
+        os.rename(PDF_FILE_NAME, pdf_title)
